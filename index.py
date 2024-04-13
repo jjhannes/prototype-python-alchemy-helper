3b02b4207d7d3347617f07623c582718cf46d3cd
jjhannes <jj.hannes.swanepoel@gmail.com>
2024-04-13 21:48:31 +0200

    first functions - main function and module syntax - get effects for ingredient - run tests
    misc
    - ignore vscode folder

--- index.py
+++ index.py
@@ -1,4 +1,17 @@
 
 from ingredientEffects import data;
 
-print(data);
+def runTests():
+    # Test get effects for ingredient
+    ingredients = getEffectsForIngredient("Kagouti Hide");
+
+    print(ingredients);
+
+def getEffectsForIngredient(ingredient):
+    return data[ingredient];
+
+def main():
+    runTests();
+
+if __name__ == "__main__":
+    main();
