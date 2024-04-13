9c12630e90a08a7b92783eedd5c448ccdc6916a1
jjhannes <jj.hannes.swanepoel@gmail.com>
2024-04-13 23:57:06 +0200

    define new function and test - determine recipe
    fixes
    - get ingredients with effects use set to keep values unique

--- index.py
+++ index.py
@@ -20,19 +20,26 @@ def runTests():
 
     print(f"[{" & ".join(scalesAndKwamaCuttle)}] has [{" & ".join(commonEffects)}] in common")
 
+    # Test determine recipe
+    desiredEffects = [ "Water Walking", "Fortify Speed" ]
+    possibleRecipes = determineRecipe(desiredEffects)
+
+    print(f"The following recipes will give you [{" & ".join(desiredEffects)}]:")
+    [print(f" - {recipe}") for recipe in possibleRecipes]
+
 def getEffectsForIngredient(ingredient):
     return data[ingredient]
 
 def getIngredientsWithEffects(effects):
-    ingredients = []
+    ingredients = set()
 
     for ingredient in data:
         ingredientEffects = data[ingredient]
 
         if any(check in ingredientEffects for check in effects):
-            ingredients.append(ingredient)      
+            ingredients.update([ ingredient ])      
 
-    return ingredients
+    return list(ingredients)
 
 def getCommonEffects(ingredients):
     commonEffects = set()
@@ -46,11 +53,59 @@ def getCommonEffects(ingredients):
             secondaryIngredientEffects = getEffectsForIngredient(secondaryIngredient)
 
             commonEffects.update(list(set(primaryIngredientEffects).intersection(secondaryIngredientEffects)))
-            
+
             # Alternative approach
             # commonEffects.update([element for element in primaryIngredientEffects if element in secondaryIngredientEffects])
 
     return list(commonEffects)
+
+def determineRecipe(desiredEffects):
+    possibleRecipes = []
+    ingredientsWithDesiredEffects = getIngredientsWithEffects(desiredEffects)
+
+    # Two ingredients
+    for primary in range(len(ingredientsWithDesiredEffects)):
+        primaryIngredient = ingredientsWithDesiredEffects[primary]
+
+        for secondary in range(primary + 1, len(ingredientsWithDesiredEffects)):
+            secondaryIngredient = ingredientsWithDesiredEffects[secondary]
+            commonEffects = getCommonEffects([ primaryIngredient, secondaryIngredient ])
+
+            if (all(item in commonEffects for item in desiredEffects)):
+                possibleRecipes.append([ primaryIngredient, secondaryIngredient ])
+
+    # Three ingredients
+    for primary in range(len(ingredientsWithDesiredEffects)):
+        primaryIngredient = ingredientsWithDesiredEffects[primary]
+
+        for secondary in range(primary + 1, len(ingredientsWithDesiredEffects)):
+            secondaryIngredient = ingredientsWithDesiredEffects[secondary]
+            
+            for tertiary in range(secondary + 1, len(ingredientsWithDesiredEffects)):
+                tertiaryIngredient = ingredientsWithDesiredEffects[tertiary]
+                commonEffects = getCommonEffects([ primaryIngredient, secondaryIngredient, tertiaryIngredient ])
+
+                if (all(item in commonEffects for item in desiredEffects)):
+                    possibleRecipes.append([ primaryIngredient, secondaryIngredient, tertiaryIngredient ])
+
+    # Four ingredients
+    for primary in range(len(ingredientsWithDesiredEffects)):
+        primaryIngredient = ingredientsWithDesiredEffects[primary]
+
+        for secondary in range(primary + 1, len(ingredientsWithDesiredEffects)):
+            secondaryIngredient = ingredientsWithDesiredEffects[secondary]
+            
+            for tertiary in range(secondary + 1, len(ingredientsWithDesiredEffects)):
+                tertiaryIngredient = ingredientsWithDesiredEffects[tertiary]
+                
+                for quaternary in range(tertiary + 1, len(ingredientsWithDesiredEffects)):
+                    quaternaryIngredient = ingredientsWithDesiredEffects[quaternary]
+                    commonEffects = getCommonEffects([ primaryIngredient, secondaryIngredient, tertiaryIngredient, quaternaryIngredient ])
+
+                    if (all(item in commonEffects for item in desiredEffects)):
+                        possibleRecipes.append([ primaryIngredient, secondaryIngredient, tertiaryIngredient, quaternaryIngredient ])
+
+    return possibleRecipes
 
 def main():
     runTests()
